5e42ea57408f6b50a8d7c98aecb9809360f7a20c
yaghoubi-mn <yaghoubi.mn@gmail.com>
2024-05-09 13:42:30 +0330

    create Book and BookCategory models

--- backend/book_store/books/models.py
+++ backend/book_store/books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    number_of_pages = models.IntegerField()
+    ImagePath = models.CharField(max_length=500, null=True)
+    sales_number = models.IntegerField(default=0)
+    description = models.TextField()
+
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField()
+
+
+class BookCategory(models.Model):
+    name = models.CharField(max_length=50)
